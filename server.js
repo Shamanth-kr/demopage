5616185b8a8386cc9fc19902527d9ad4dc58a370
Shamanth-kr <shamanthkr11@gmail.com>
2025-07-15 13:58:32 +0530

    Initial setup with auth routes

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Import Routes
+app.use('/auth', require('./routes/auth'));
+
+// Start server
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch(err => console.log(err));
